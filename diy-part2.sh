1a21ca79da3c03777091fb4c2d92566fc8f67f88
tcsr200722 <350524166@qq.com>
2021-01-03 12:47:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,28 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+add koolproxyR 
+git clone https://github.com/Beginner-Go/luci-app-koolproxyR.git package/luci-app-koolproxyR
+add luci-app-adbyby-plus
+git clone https://github.com/tcsr200722/app-adbyby.git package/luci-app-adbyby-plus
+add luci-app-ledeproxy
+git clone https://github.com/tcsr200722/luci-app-ledeproxy-2.git package/luci-app-ledeproxy
+add luci-app-filebrowser
+git clone https://github.com/tcsr200722/luci-app-filebrowser.git package/luci-app-filebrowser
+add luci-app-dockerman
+#git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+#git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
+add luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+add openwrt-upx
+git clone https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
+add luci-app-jd-dailybonus
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+add luci-app-diskman
+mkdir -p package/luci-app-diskman && \
+wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O package/luci-app-diskman/Makefile
+mkdir -p package/parted && \
+wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
+./scripts/feeds update -a
+./scripts/feeds install -a
